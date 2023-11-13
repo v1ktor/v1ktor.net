3de49290cd317f2a0cf950b1baae6bf51f64801b
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-11-14 00:19:44 +0200

    Remove unnecessary semicolon

--- eleventy.config.drafts.js
+++ eleventy.config.drafts.js
@@ -9,7 +9,7 @@ function eleventyComputedPermalink() {
 
 		return data.permalink;
 	}
-};
+}
 
 function eleventyComputedExcludeFromCollections() {
 	// When using `addGlobalData` and you *want* to return a function, you must nest functions like this.
@@ -22,7 +22,7 @@ function eleventyComputedExcludeFromCollections() {
 
 		return data.eleventyExcludeFromCollections;
 	}
-};
+}
 
 module.exports.eleventyComputedPermalink = eleventyComputedPermalink;
 module.exports.eleventyComputedExcludeFromCollections = eleventyComputedExcludeFromCollections;
